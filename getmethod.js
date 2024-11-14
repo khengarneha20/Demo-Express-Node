1c3ddd05255553c934ea7e5b302222e2e6167286
khengarneha20 <nkhengar@woyce.io>
2024-11-14 20:03:31 +0530

    commit1

--- getmethod.js
+++ getmethod.js
@@ -4,12 +4,16 @@ var app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan')
 
+// Get method--------------------->>
+
 app.get('/process_get', function (req, res) {
    res.send('Get Request Called');
 })
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Post method--------------------->>
 
 app.post('/process_post', function (req, res) {
  //  res.send('Post request called ');
@@ -21,6 +25,14 @@ app.post('/process_post', function (req, res) {
 
 });
 
+// Put Method --------------------------->> 
+
+// data in postman {
+//    "id" : 4,
+//    "name" : "neha khengar",
+//    "mno" : 9099193215,
+//    "add" : "rajkot"
+//    } 
 app.put('/process_put', function (req, res){
 
    const data = req.body;
@@ -43,14 +55,29 @@ else {
     });
 }
 
-
-   // let users = [
-   //    {id : 1, name : "crish"},
-   //    {id : 2, name : "gell"}
-   // ]
-
-
 });
+
+// Delete method ----------->>
+
+app.delete('/delete', function(req,res){
+
+   const data = req.body;
+   const {id ,name, mno , add} = data;
+
+   if (id === 1){
+
+      res.json({
+         success : true,
+         message : "This is Example of delete api",
+         message : `The user with this id ${id} is deleted successfully` 
+   
+      }); 
+   }
+
+
+})
+
+
 
 
 
